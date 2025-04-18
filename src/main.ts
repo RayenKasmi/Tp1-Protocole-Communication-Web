4177b7027ed3a345c7f1cbf1e8736b2c8d048e8c
iyed <iyedabdelli73@gmail.com>
2025-04-18 12:31:00 +0100

    Add pagination

--- src/main.ts
+++ src/main.ts
@@ -22,7 +22,7 @@ async function bootstrap() {
   app.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
   
   app.useStaticAssets(join(__dirname, '..', 'public'));
-  
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
