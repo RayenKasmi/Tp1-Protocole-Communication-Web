67deae6dadf3ee7bc986a97c27c32dcd0ae8cc38
RayenKasmi <146983595+RayenKasmi@users.noreply.github.com>
2025-04-19 02:27:28 +0100

    Admin guard (#6)
    * added Role Guard with testing routes in quth controller
    
    * merged with auth module

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,8 +1,10 @@
-import { Body, Controller, Post, UseGuards, Request } from '@nestjs/common';
+import { Body, Controller, Post, UseGuards, Request, Get } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { RegisterDto } from './dto/register.dto';
 import { LoginDto } from './dto/login.dto';
 import { JwtAuthGuard } from './guards/jwt-auth.guard';
+import { RolesGuard } from 'src/common/guards/roles.guard';
+import { Roles } from 'src/common/decorators/roles.decorator';
 import { GetUser } from '../common/decorators/get-user.decorator';
 
 @Controller('auth')
@@ -36,4 +38,27 @@ export class AuthController {
             user: user 
           };
     }
+
+    @UseGuards(JwtAuthGuard,RolesGuard)
+    @Roles('admin')
+    @Get('admin')
+    async admin( @GetUser() user: any)
+    {
+        return { 
+            message: 'Only admins can access this endpoint', 
+            user: user 
+          };
+    }
+
+    @UseGuards(JwtAuthGuard,RolesGuard)
+    @Roles('admin', 'user')
+    @Get('admin_user')
+    async admin_user( @Request() req: any) 
+    {
+        return { 
+            message: 'both admins and users can access this endpoint', 
+            user: req.user 
+          };
+    }
+
 }
